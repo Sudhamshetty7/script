ac159656d2b4b4482f9da4e0615fdd42626fd2e2
Ubuntu <ubuntu@ip-172-31-32-16.ap-south-1.compute.internal>
2024-01-11 08:46:22 +0000

    added two scripts

---
+++ lararr.sh
@@ -0,0 +1,20 @@
+write a scirpt to print the largest number in an array
+#!/bin/bash
+a="5 9 6 3 5 4 1 7 19 5"
+count=1
+for i in $a
+do
+if [ $count -eq 1 ]
+then
+num=$i
+fi
+if [ $num -gt $i ]
+then
+largest=$num
+else
+largest=$i
+num=$i
+fi
+count=`expr $count + 1`
+done
+echo "The largest of number of the array is $num"
---
+++ larnum.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ $a -gt $b ]
+then
+echo "$a is the greater"	
+else
+echo "$b is the gretater number"\
+fi	
